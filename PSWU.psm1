59b66ff8fafd02c74271eea7be29d0ca73f08dbd
bklockwood <bryan@lockwood.us>
2015-03-30 04:48:16 -0700

    Remove install-allupdates. Resolve schtasks issue
    Schtasks needs some funky escaping to work if there are spaces in path to executable or arguments.
    install-allupdates is moved out to its own script file.

--- PSWU.psm1
+++ PSWU.psm1
@@ -1,62 +1,4 @@
-#requires -version 3.0
-
-function Install-AllUpdates {
-    #flowchart: http://i.imgur.com/NSV8AH2.png
-
-    [string]$ScriptName = $($MyInvocation.MyCommand.Name)
-    [string]$ScriptName = $($ScriptName.Split('.')[0])
-    [string]$ScriptPath = $PSCommandPath
-    $Logfile = "$env:PUBLIC\Desktop\$ScriptName.log"
-    Write-Log $Logfile " -=-=-=-=-=-=-=-=-=-=-=-"
-    Write-Log $Logfile "PSWU system patcher is starting (as $env:username)."
-
-    Write-Log $Logfile "Starting PSWU function 'Install-AllUpdates'"
-    if (!(Test-AdminPrivs)) {
-        Write-Warning "You must elevate to Admin privs to download or install updates"
-        Write-Log $Logfile "You must elevate to Admin privs to download or install updates"
-        break 
-    }
-
-    if (Test-RebootNeeded) {
-        Write-Log $Logfile "Restart needed (for pending Windows Updates)."
-        if (!(CheckForScheduledTask "PSWU")) {ScheduleRerunTask "PSWU" $ScriptPath}
-        Write-Log $Logfile "Restarting in 15 seconds!"
-        Start-Sleep -Seconds 15
-        Restart-Computer -Force
-        break #Without this, script will continue processing during the shutdown.
-    } else {
-        Write-Log $Logfile "No reboot needed."
-    }
-
-    Write-Log $Logfile "Checking for updates."
-    $ISearchResult = Get-UpdateList
-
-    if ($ISearchResult.ResultCode -eq 2) {
-        Write-Log $Logfile "Successfully retreived update list"
-        if ($ISearchResult.Updates.Count -gt 0) {
-            [string]$UpdateReport = Show-UpdateList -ISearchResult $ISearchResult
-            Write-Log $Logfile $UpdateReport  
-            Write-Log $Logfile "Downloading and installing $($ISearchResult.Updates.Count) updates."
-            $Install = Install-Update -ISearchResult $ISearchResult -Verbose
-            Write-Log $Logfile "Done installing updates. Restarting script to check for more."
-            Install-AllUpdates
-        } else {
-            Write-Log $Logfile "Windows is up to date; script cleaning up."
-            #check for PSWU Scheduled Task and delete if found
-           if (CheckForScheduledTask "PSWU") {
-                Write-Log $Logfile "Found PSWU task; removing. "
-                #Tried Stop-SceduledTask and Unregister-ScheduledTask
-                #Nither will kill the running task. schtasks works.
-                schtasks /delete /tn pswu /F
-                }
-		    #TODO: tell user all done. write to all-users desktop?     
-            Write-Log $Logfile "Cleanup complete. Running as $env:username - script exiting."
-            New-Item -ItemType File -Path "$env:PUBLIC\Desktop" -Name "DONE UPDATING" -Value "You can delete this file and $Logfile"
-            break
-        }
-    }
-}
-
+#requires -version 2.0
 
 <#
 .Synopsis
@@ -162,8 +104,10 @@ function ScheduleRerunTask ($TaskName, $ScriptPath)
 {
     schtasks /create `
         /RU SYSTEM `
-        /SC onstart /TN $TaskName `
-        /TR "$PSHome\powershell.exe -executionPolicy Unrestricted -File $ScriptPath" 
+        /SC onstart /TN $TaskName /RL HIGHEST `
+        #note the funky escaping because of http://goo.gl/SgSLrQ
+        /TR "\`"$PSHome\powershell.exe\`" -executionPolicy Unrestricted -File \`"$ScriptPath\`""
+        #/TR "$PSHome\powershell.exe -executionPolicy Unrestricted -File \`"$ScriptPath\`""
 }
 
 <#
