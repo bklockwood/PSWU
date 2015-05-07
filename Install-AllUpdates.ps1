cf647d313dac8d710c4fdfdb26013f6f4b1fcfda
bklockwood <bryan@lockwood.us>
2015-05-06 22:37:13 -0700

    resolves #15

--- Install-AllUpdates.ps1
+++ Install-AllUpdates.ps1
@@ -1,3 +1,14 @@
 #requires -version 2.0
 
-Install-AllUpdates
+[string]$ScriptName = $($MyInvocation.MyCommand.Name)
+[string]$ScriptPath = split-path $SCRIPT:MyInvocation.MyCommand.Path 
+[string]$ScriptFullPath = $SCRIPT:MyInvocation.MyCommand.Path
+try {    
+    import-module -name $ScriptPath   
+} catch {
+    $Logtext = "Could not import the PSWU module; exiting."
+    Out-file -FilePath $Logfile -Append -NoClobber -InputObject $Logtext -Encoding ascii
+    break
+}   
+
+Install-AllUpdates -ScriptName $ScriptName -Scriptpath $ScriptPath -ScriptFullName $ScriptFullPath -Verbose
